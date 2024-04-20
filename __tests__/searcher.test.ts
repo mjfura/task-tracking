52c4431f11b447a80293b6a2543c99e75da230f3
Marco Fura <mfura@kataricorp.com>
2024-04-19 22:55:04 -0400

    refactor: add variables for test

--- __tests__/searcher.test.ts
+++ __tests__/searcher.test.ts
@@ -1,6 +1,8 @@
 import { searcher } from '../src/helpers/searher'
 
 it('Should return true if a string includes another string', () => {
-  const result = searcher('hello', 'he')
+  const keyword = 'he'
+  const text = 'hello'
+  const result = searcher(text, keyword)
   expect(result).toBe(true)
 })
